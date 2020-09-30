d5594d88100ff83764c780be022836ec8ec8fb6d
vagrant <vagrant@vagrant.vm>
2020-09-30 19:53:41 +0000

    defined square function

--- arithmetic.py
+++ arithmetic.py
@@ -22,13 +22,14 @@ def divide(num1, num2):
 
     return num1 / num2
 
-
 def square(num1):
     """Return the square of the input."""
 
+    return num1 * num1
 
 def cube(num1):
     """Return the cube of the input."""
+
 
 
 def power(num1, num2):
